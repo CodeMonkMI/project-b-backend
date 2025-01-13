26813a9cacc81fcddd53e174b3be9a12f93787c7
Monirul Islam <mmislam027@gmail.com>
2025-01-14 01:53:20 +0600

    update history create route

--- services/history/src/controllers/createHistory.ts
+++ services/history/src/controllers/createHistory.ts
@@ -1,8 +1,7 @@
-import { REQUEST_SERVICE } from "@/config";
 import prisma from "@/prisma";
 import { CreateDonationHistoryDTOSchema } from "@/schemas";
 import invalidRequestIdError from "@/utils/invalidRequestError";
-import axios, { AxiosError } from "axios";
+import { AxiosError } from "axios";
 import { NextFunction, Request, Response } from "express";
 
 export const createHistory = async (
@@ -15,10 +14,6 @@ export const createHistory = async (
     if (!parsedData.success) {
       return res.status(400).json({ errors: parsedData.error.errors });
     }
-
-    await axios.get(
-      `${REQUEST_SERVICE}/request/details/${parsedData.data.requestId}`
-    );
 
     const data = await prisma.donationHistory.create({
       data: {
